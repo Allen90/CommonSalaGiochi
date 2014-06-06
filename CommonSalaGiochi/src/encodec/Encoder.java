a8b889264a36dcd8ce90ddb542e8537f149ebee6
Allen90 <90allen90@gmail.com>
2014-06-06 14:24:08 +0200

    Primo metodo encoder

--- CommonSalaGiochi/src/encodec/Encoder.java
+++ CommonSalaGiochi/src/encodec/Encoder.java
@@ -1,9 +1,23 @@
 package encodec;
 
+import rubamazzo.Carta;
+import rubamazzo.SituazioneRubamazzo;
+
 public class Encoder {
 
 	public static final String ok = "OK#";
 	public static final String ko = "KO#";
 	
+	public static String aggiornaRubamazzo(SituazioneRubamazzo s){
+		String output = new String(ok);
+		for(Carta c : s.getMano())
+			output += c.toString() + "#";
+		for(Carta c : s.getBottini())
+			output += c.toString() + "#";
+		for(Carta c : s.getBanco())
+			output +=c.toString() + "#";
+		output += s.getAbilitato() + "\n";
+		return output;
+	}
 	
 }
