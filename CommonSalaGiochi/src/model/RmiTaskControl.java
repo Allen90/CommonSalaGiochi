29a69fb24e6e9bf2e4c2699f835399b86ec0ea52
Fritz90 <xxfritz.90xx@gmail.com>
2014-06-06 15:21:54 +0200

    fixed rmi task interface

--- CommonSalaGiochi/src/model/RmiTaskControl.java
+++ CommonSalaGiochi/src/model/RmiTaskControl.java
@@ -3,14 +3,16 @@ package model;
 import java.rmi.Remote;
 import java.util.ArrayList;
 
+import rubamazzo.SituazioneRubamazzo;
 import rubamazzo.TavoloRubamazzo;
 import slot.Rollata;
 import tombola.GiocatoreTombola;
+import tombola.SituazioneTombola;
 
 public interface RmiTaskControl extends Remote{
 	public Rollata rolla();
-	public GiocatoreTombola aggTombola();
-	public TavoloRubamazzo aggRubaMazzo();
+	public SituazioneTombola aggTombola();
+	public SituazioneRubamazzo aggRubaMazzo();
 	public ArrayList<Utente> aggClass();
 	public ArrayList<Utente> aggClassGiorn();
 	public void termina();
